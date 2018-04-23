c6db410dce0a61528cc955a95aee4a0ef8b24468
viccycg <viccyg@digitalpharos.com>
2018-04-23 15:11:30 -0600

    Created songs component

---
+++ src/app/components/songs/songs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'songs',
+  templateUrl: './songs.component.html',
+  styleUrls: ['./songs.component.css']
+})
+export class SongsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
