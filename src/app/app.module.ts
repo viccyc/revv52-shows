e7038cef2e872c76d27aba5c95312ebfd4bc4a00
viccycg <viccyg@digitalpharos.com>
2018-04-23 15:11:09 -0600

    inital commit and skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SongsComponent } from './components/songs/songs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongsComponent
   ],
   imports: [
     BrowserModule
