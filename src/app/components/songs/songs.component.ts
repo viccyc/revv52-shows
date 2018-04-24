bbfa54abe740f193dc50a15365b52cdcc376eca7
viccycg <viccyg@digitalpharos.com>
2018-04-24 17:05:13 -0600

    Updated the songs component to use the route to detail/song.id

--- src/app/components/songs/songs.component.ts
+++ src/app/components/songs/songs.component.ts
@@ -10,27 +10,15 @@ import { SongService } from '../../services/song/song.service';
 })
 export class SongsComponent implements OnInit {
 
-  // songs = SONGS;
   songs: Song[];
 
-  selectedSong: Song;
-
-  constructor(private songService: SongService) {
-
-  }
+  constructor(private songService: SongService) { }
 
   ngOnInit() {
     this.getSongs();
   }
 
-  onSelect(song: Song): void {
-    this.selectedSong = song;
-  }
-
   getSongs(): void {
-    // assigns an array of songs to the component's songs property. (synchronous)
-    // this.songs = this.songService.getSongs();
-
     // waits for the Observable to emit the array of songs.
     // Then passes the emitted array to the callback, which sets the component's songs property. (asynchronous)
     this.songService.getSongs()
