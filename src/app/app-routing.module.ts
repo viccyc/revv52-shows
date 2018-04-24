6647b2dd989ac12174e01885bb0362669cbdfbad
viccycg <viccyg@digitalpharos.com>
2018-04-24 16:43:58 -0600

    Added route for the dashboard to link to the song id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SongsComponent } from './components/songs/songs.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SongDetailComponent } from './components/song-detail/song-detail.component';
 
 // path: a string that matches the URL in the browser address bar.
 // component: the component that the router should create when navigating to this route.
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'songs', component: SongsComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: 'detail/:id', component: SongDetailComponent }
 ]
 
 @NgModule({
