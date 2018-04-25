5c2bf1c72d653396f71102303e52766b2a951668
viccycg <viccyg@digitalpharos.com>
2018-04-25 11:04:01 -0600

    Setting up http and in memory data service for testing. Deleted mock data, because of DB creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-datas/in-memory-data.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SongsComponent } from './components/songs/songs.component';
 import { SongDetailComponent } from './components/song-detail/song-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 import { SongService } from './services/song/song.service';
-import { MessagesComponent } from './components/messages/messages.component';
 import { MessageService } from './services/messages/message.service';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
@@ -25,7 +27,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // TODO: Remove this when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [
     SongService,
