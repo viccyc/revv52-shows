85b1d7ebc718a498ef254215f0bf96fa96a85198
viccycg <viccyg@digitalpharos.com>
2018-04-25 12:07:32 -0600

    Update getSong to use http.  Handle errors.

--- src/app/services/song/song.service.ts
+++ src/app/services/song/song.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Song } from '../../components/songs/songs';
 
@@ -25,11 +26,25 @@ export class SongService {
   // makes it asynchronous - returns an Observable that emits a single value, the array of mock songs when done 
   getSongs(): Observable<Song[]> {
     return this.http.get<Song[]>(this.songsUrl)
+      .pipe(
+        tap(songs => this.log(`fetched songs`)),
+        catchError(this.handleError('getSongs', []))
+      );
   }
 
   getSong(id: number): Observable<Song> {
-    // TODO: send the message _after_ fetching the song
-    this.messageService.add(`SongService: fetched song id=${id}`);
-    // return of(SONGS.find(song => song.id === id));
+    const url = `${this.songsUrl}/${id}`;
+    return this.http.get<Song>(url).pipe(
+      tap(_ => this.log(`fetched song id=${id}`)),
+      catchError(this.handleError<Song>(`getSong id=${id}`))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
