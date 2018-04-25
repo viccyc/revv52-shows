9fbf4b76c182018c6c4ea8615cb3b9b07922e3bb
viccycg <viccyg@digitalpharos.com>
2018-04-25 09:29:09 -0600

    Added a router link to get song by id and display details.

--- src/app/components/song-detail/song-detail.component.ts
+++ src/app/components/song-detail/song-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Song } from '../songs/songs';
+import { SongService } from '../../services/song/song.service';
 
 
 @Component({
@@ -11,9 +15,23 @@ export class SongDetailComponent implements OnInit {
   // @Input decorator to make the song property available for binding by the external SongComponent.
   @Input()song: Song;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private songService: SongService,
+    private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getSong();
   }
 
+  getSong(): void {
+    const id = +this.route.snapshot.paramMap.get('id');  //+ converts string to number
+    this.songService.getSong(id)
+      .subscribe(song => this.song = song);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/services/song/song.service.ts
+++ src/app/services/song/song.service.ts
@@ -16,8 +16,14 @@ export class SongService {
   }
   // makes it asynchronous - returns an Observable that emits a single value, the array of mock songs when done 
   getSongs(): Observable<Song[]> {
-    // TODO: send the message _after_ fetching the heroes
+    // TODO: send the message _after_ fetching the songs
     this.messageService.add('SongService: fetched songs');
     return of(SONGS);
+  }
+
+  getSong(id: number): Observable<Song> {
+    // TODO: send the message _after_ fetching the song
+    this.messageService.add(`SongService: fetched song id=${id}`);
+    return of(SONGS.find(song => song.id === id));
   }
 }
