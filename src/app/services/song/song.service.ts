164077829493b4ae8b545d55d2b4b0ef9519a687
viccycg <viccyg@digitalpharos.com>
2018-04-25 11:30:52 -0600

    Used http client to get the songs from the server

--- src/app/services/song/song.service.ts
+++ src/app/services/song/song.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Song } from '../../components/songs/songs';
-import { SONGS } from '../../mock-songs';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
+import { Song } from '../../components/songs/songs';
 
 import { MessageService } from '../messages/message.service';
 
@@ -11,19 +11,25 @@ import { MessageService } from '../messages/message.service';
 export class SongService {
 
   // typical "service-in-service" scenario: you inject the MessageService into the SongService which is injected into the SongComponent.
-  constructor(private messageService: MessageService) { 
-
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+  
+  // Log a SongService message with the MessageService
+  private log(message: string) {
+    this.messageService.add('SongService: ' + message);
   }
+
+  private songsUrl = 'api/songs';
+
   // makes it asynchronous - returns an Observable that emits a single value, the array of mock songs when done 
   getSongs(): Observable<Song[]> {
-    // TODO: send the message _after_ fetching the songs
-    this.messageService.add('SongService: fetched songs');
-    return of(SONGS);
+    return this.http.get<Song[]>(this.songsUrl)
   }
 
   getSong(id: number): Observable<Song> {
     // TODO: send the message _after_ fetching the song
     this.messageService.add(`SongService: fetched song id=${id}`);
-    return of(SONGS.find(song => song.id === id));
+    // return of(SONGS.find(song => song.id === id));
   }
 }
