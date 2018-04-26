48314eb00321e31b2ddb016c1d9aceb51fc687d0
viccycg <viccyg@digitalpharos.com>
2018-04-26 09:55:57 -0600

    Created search which searches for matching songs on each keystroke, using AsyncPipe and observable stream

--- src/app/components/song-search/song-search.component.ts
+++ src/app/components/song-search/song-search.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { of } from 'rxjs/observable/of';
+
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+import { Song } from '../songs/songs';
+import { SongService } from '../../services/song/song.service';
+
 @Component({
   selector: 'song-search',
   templateUrl: './song-search.component.html',
   styleUrls: ['./song-search.component.css']
 })
 export class SongSearchComponent implements OnInit {
+  songs$: Observable<Song[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private songService: SongService) { }
 
-  ngOnInit() {
+  // push a search term into the observable stream
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.songs$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+      // ignore new term is same as previous term
+      distinctUntilChanged(),
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.songService.searchSongs(term)),
+    );
+  }
 }
